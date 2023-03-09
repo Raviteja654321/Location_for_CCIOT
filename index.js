19155a69cbdcf40e4e6facd062391c0a4ff3d8e3
Raviteja654321 <94986564+Raviteja654321@users.noreply.github.com>
2023-03-10 00:09:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// Replace YOUR_AUTH_TOKEN with your Blynk project's Auth Token
+const url = `http://cloud.blynk.cc:8080/YOUR_AUTH_TOKEN/update/v1`;
+
+// Get the current location of the device
+navigator.geolocation.getCurrentPosition(position => {
+  const { latitude, longitude } = position.coords;
+  console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+
+  // Send the location data to Blynk using the Virtual Pin API
+  const data = `${latitude},${longitude}`;
+  fetch(`${url}?value=${data}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' }
+  })
+  .then(response => console.log(response))
+  .catch(error => console.error(error));
+}, error => {
+  console.error(`Error getting location: ${error.message}`);
+});
