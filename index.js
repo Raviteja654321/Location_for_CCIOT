93c83f5e91d3a2e09d5a944d0107103c8ddc9fd1
Raviteja654321 <94986564+Raviteja654321@users.noreply.github.com>
2023-03-10 00:19:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-// Replace YOUR_AUTH_TOKEN with your Blynk project's Auth Token
-const url = `http://cloud.blynk.cc:8080/Le1pNdDBrs0jBm2qL3VKWulK3QBCF7ix/update/v1`;
+const url = 'http://cloud.blynk.cc:8080/Le1pNdDBrs0jBm2qL3VKWulK3QBCF7ix/update/V1';
 
-// Get the current location of the device
 navigator.geolocation.getCurrentPosition(position => {
   const { latitude, longitude } = position.coords;
   console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
-
-  // Send the location data to Blynk using the Virtual Pin API
+  
   const data = `${latitude},${longitude}`;
-  fetch(`${url}?value=${data}`, {
+  
+  fetch(url, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' }
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify([data])
   })
   .then(response => console.log(response))
   .catch(error => console.error(error));
